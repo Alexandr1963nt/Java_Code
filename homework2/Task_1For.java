9bd8013468738a7b283ebce301d21cc49a47211c
Alexandr1963nt <Alexander.Trofimenko@asapcg.com>
2023-02-15 16:50:03 +0300

    Добавил решение через for

---
+++ homework2/Task_1For.java
@@ -0,0 +1,46 @@
+package homework2;
+
+public class Task_1For {
+
+    static String delSpaceAddPoint(String str) {
+        StringBuilder strBuild = new StringBuilder(str.trim());
+        char zero = strBuild.charAt(0);
+        if (Character.isLowerCase(zero))
+            strBuild.setCharAt(0, Character.toUpperCase(zero));
+        int start = -1;
+        int end = -1;
+
+        for (int i = 1; i < strBuild.length() - 1; i++) {
+            zero = strBuild.charAt(i); // После убрать
+            start = strBuild.charAt(i) == ' ' &
+                    start < 0 &
+                    strBuild.charAt(i + 1) == ' ' ? i + 1 : start;
+            end = start > 0 & strBuild.charAt(i) == ' ' ? i + 1 : end;
+
+            if (strBuild.charAt(i) != ' ' & end > 0) {
+                strBuild.delete(start, end);
+                i -= end - start; 
+                start = -1;
+                end = -1;
+            }
+        }
+
+        for (int i = 0; i < strBuild.length() - 1; i++) {
+            if (strBuild.charAt(i) == ' ' && Character.isUpperCase(strBuild.charAt(i + 1))) {
+                strBuild.insert(i, '.');
+                i++;
+            }
+        }
+        strBuild.append('.');
+        return str = strBuild.toString();
+    }
+
+    public static void main(String[] args) {
+
+        String question = "   how you going  I'm    splended  by java :))   Thanks   ";
+        System.out.println(question);
+        System.out.println(delSpaceAddPoint(question));
+
+    }
+
+}
